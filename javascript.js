273879759deecd77d6c3e2de96f44c10bbc45d07
Davitbakradze <davitibaqradze98@gmail.com>
2024-01-18 13:39:56 +0400

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,67 @@
+// simple variebles //
+
+var name = "Giorgi";
+var age = 25;
+var isStudent = true;
+
+let city = "Tbilisi";
+let temperature = 28;
+
+const PI = 3.14;
+const MAX_VALUE = 100;
+
+
+// simple variebles //
+
+// math operations //
+
+let x=30;
+let y=15;
+let c=25;
+let z=28;
+
+let n=x+y;
+console.log(n);
+//result 45//
+
+let m=x*y;
+console.log(m);
+//result 450//
+
+let f=z-c;
+console.log(f);
+//result 3//
+
+let g=z/c;
+console.log(g);
+//result 1.12//
+
+// math operations //
+
+//string concatenation//
+
+console.log(name + " is " + age + " years old ")
+//Giorgi is 25 years old//
+
+let sentence = `${name}  lives in ${city}`;
+console.log(sentence);
+//Giorgi  lives in Tbilisi//
+
+//string concatenation//
+
+//Area of a rectangle//
+
+let w=7;
+    e=5;
+
+let q=w*e;
+console.log(q);
+
+//result 35//
+
+//Area of a rectangle//
+
+
+
+
+
